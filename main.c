0012a93b459f09f8cf040e4d3261abb604713185
Никита Левашов <n.levashov@inbox.ru>
2023-09-05 20:43:12 +0300

    struct Node

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Структура узла бинарного дерева
+typedef struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+
+int main() {
+
+
+    return 0;
+}
